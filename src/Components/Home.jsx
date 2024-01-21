ae81319c81f87b5bfcc249adfba6cb8862b502c6
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-01-21 22:12:58 +0430

    Style: Home component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <main>
+      <section className="max-w-7xl mx-auto h-[80vh] flex flex-col justify-center items-center px-6">
+        <h1 className="text-3xl md:text-5xl font-bold">
+          جواهر گلدن دنیایی از <span className="text-yellow-400">جواهرات</span>{" "}
+        </h1>
+        <p className="text-slate-600 md:text-xl mt-4">
+          بهترین جواهرات را به بهترین قیمت از ما دریافت نمایید
+        </p>
+        <Link
+          to="/rings"
+          className="transition text-white bg-yellow-500 hover:bg-yellow-400 py-2 px-4 rounded-lg mt-10"
+        >
+          مشاهده جواهرات
+        </Link>
+      </section>
+    </main>
+  );
 }
 
 export default Home;
