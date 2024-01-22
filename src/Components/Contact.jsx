5aca960c163dcc8d4cb929dadcd4a381819163ab
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-01-22 14:19:23 +0430

    Style: Contact section

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,17 +3,17 @@ function Contact() {
     <section>
       <div className="max-w-7xl mx-auto h-96 p-4">
         <h2 className="font-semibold border-r-4 border-yellow-500 text-right text-3xl md:text-4xl m-6 pr-3 py-2">
-          ارتباط با ما
+          Contact
         </h2>
 
-        <div className="flex flex-col mt-20 gap-2 text-center md:text-2xl">
+        <div className="flex flex-col mt-20 gap-5 text-center md:text-2xl">
           <p className="text-slate-600">
+            <span className="font-bold text-slate-800">Email: </span>
             example@gmail.com{" "}
-            <span className="font-bold text-slate-800">: ایمیل</span>
           </p>
           <p className="text-slate-600">
-            ۰۰۹۳۷۰۰۰۰۰۰{" "}
-            <span className="font-bold text-slate-800">: واتسپ</span>
+            <span className="font-bold text-slate-800">Whatsapp: </span>
+            +93 700 0000{" "}
           </p>
         </div>
       </div>
