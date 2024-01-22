c85ce399b62af415cef78eb4451532cbd0b5ec41
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-01-22 17:02:27 +0430

    Style: Navbar responsive

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer className="bg-slate-200">
-      <div className="max-w-7xl h-20 flex flex-col justify-center items-center gap-7 mx-auto px-4">
-        <div className="flex justify-center items-center gap-7 bg-slate-200 mx-auto px-4">
+      <div className="max-w-7xl h-20 flex flex-col justify-center items-center mx-auto px-4">
+        <div className="flex flex-col justify-center items-center gap-2 bg-slate-200 mx-auto px-4">
           <h1 className="font-bold text-lg">
             <span className="text-yellow-400">Golden</span> Jewelery
           </h1>
-          <div className="w-[1px] h-6 bg-slate-400"></div>
+          {/* <div className="w-[1px] h-6 bg-slate-400"></div> */}
           <div className="flex gap-5 items-center text-sm text-slate-500 font-medium">
             <Link to="/" className="transition hover:text-black">
               Home
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+import { FaBagShopping } from "react-icons/fa6";
+import { FaBars } from "react-icons/fa6";
+
 function Navbar() {
+  const [mobileMenu, setMobileMenu] = useState(false);
   return (
     <nav className="border-b">
       <div className="max-w-7xl mx-auto h-20 px-5 flex justify-between items-center">
@@ -8,7 +13,7 @@ function Navbar() {
           <span className="text-yellow-400">Golden</span> Jewelery
         </h1>
 
-        <div className="flex gap-5 items-center text-sm text-gray-600 font-semibold uppercase">
+        <div className="hidden sm:flex gap-5 items-center text-sm text-gray-600 font-semibold uppercase">
           <Link to="/" className="transition hover:text-black">
             Home
           </Link>
@@ -16,10 +21,25 @@ function Navbar() {
             Rings
           </Link>
           <Link
+            to="/cart"
+            className="transition hover:text-black flex items-center gap-2"
+          >
+            Cart <FaBagShopping />
+          </Link>
+          <Link
             to="/contact"
             className="transition text-white bg-yellow-500 hover:bg-yellow-400 py-2 px-4 rounded-lg"
           >
             Contact
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-4 sm:hidden">
+          <button>
+            <FaBars className="text-xl" />
+          </button>
+          <Link to="/cart">
+            <FaBagShopping className="text-xl" />
           </Link>
         </div>
       </div>
