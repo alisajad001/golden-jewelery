6f99cd55eb283ed9d2076a1c5a0f9e981d9b9629
alisajad001 <forwebonly0@gmail.com>
2024-01-23 00:44:10 +0430

    Style: Footer & fix typo

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,26 +2,13 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="bg-slate-200">
-      <div className="max-w-7xl h-20 flex flex-col justify-center items-center mx-auto px-4">
-        <div className="flex flex-col justify-center items-center gap-2 bg-slate-200 mx-auto px-4">
-          <h1 className="font-bold text-lg">
-            <span className="text-yellow-400">Golden</span> Jewelery
-          </h1>
-          {/* <div className="w-[1px] h-6 bg-slate-400"></div> */}
-          <div className="flex gap-5 items-center text-sm text-slate-500 font-medium">
-            <Link to="/" className="transition hover:text-black">
-              Home
-            </Link>
-            <Link to="/rings" className="transition hover:text-black">
-              Rings
-            </Link>
-            <Link to="/contact" className="transition hover:text-black">
-              Contact
-            </Link>
-          </div>
-        </div>
-      </div>
+    <footer className="text-center py-5 flex flex-col gap-2">
+      <h1 className="font-bold text-lg">
+        <span className="text-yellow-400">Pathan</span> Jewelery
+      </h1>
+      <p className="text-sm text-slate-600">
+        Copyright © 2024 Patahan Jewelery
+      </p>
     </footer>
   );
 }
