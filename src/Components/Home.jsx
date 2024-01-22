c4b2d1a44526c24ad5d883f5554431e9f230f937
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-01-22 14:13:23 +0430

    refactor: Language change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,19 +2,20 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <main>
+    <main className="main-section">
       <section className="max-w-7xl mx-auto h-screen flex flex-col justify-center items-center px-6">
-        <h1 className="text-3xl md:text-5xl font-bold">
-          جواهر گلدن دنیایی از <span className="text-yellow-400">جواهرات</span>{" "}
+        <h1 className="text-3xl md:text-6xl font-bold text-white">
+          Welcome to <span className="text-yellow-400">Golden</span> Jewelery
         </h1>
-        <p className="text-slate-600 md:text-xl mt-4">
-          بهترین جواهرات را به بهترین قیمت از ما دریافت نمایید
+        <p className="text-slate-200 max-w-2xl md:text-center md:text-xl mt-6">
+          Find the best jewelery for you and your family. We have a wide range
+          of rings, necklaces, earrings, bracelets and more.
         </p>
         <Link
           to="/rings"
           className="transition text-white bg-yellow-500 hover:bg-yellow-400 py-2 px-4 rounded-lg mt-10"
         >
-          مشاهده جواهرات
+          Shop Now
         </Link>
       </section>
     </main>
