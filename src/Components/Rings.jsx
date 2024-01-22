1cf11773a77b3cbe9d8f224bef185fbbd7e3f4ca
alisajad001 <forwebonly0@gmail.com>
2024-01-23 00:47:36 +0430

    Style: Rings heading

--- src/Components/Rings.jsx
+++ src/Components/Rings.jsx
@@ -4,7 +4,7 @@ function Rings() {
   return (
     <section>
       <div className="max-w-7xl flex flex-col p-4 mx-auto mb-20">
-        <h2 className="font-semibold border-r-4 border-yellow-500 text-right text-3xl md:text-4xl m-6 pr-3 py-2">
+        <h2 className="font-semibold border-l-4 border-yellow-400 text-3xl md:text-4xl m-3 pl-3">
           Rings
         </h2>
 
