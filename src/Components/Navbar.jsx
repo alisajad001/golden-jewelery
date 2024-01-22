5ec036a355b983ac20af52cc851f3d246710e219
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-01-22 17:18:06 +0430

    fix: removed Fax

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-import { FaBagShopping, FaX, FaXmark } from "react-icons/fa6";
+import { FaBagShopping, FaXmark } from "react-icons/fa6";
 import { FaBars } from "react-icons/fa6";
 
 function Navbar() {
